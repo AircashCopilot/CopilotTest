570ad8fef5e923db786d931a4961e656dd390d24
Bernard Kovač <bernard.kovac@aircash.eu>
2025-09-19 22:44:58 +0200

    sdfsfd

--- CopilotTest/Form1.cs
+++ CopilotTest/Form1.cs
@@ -12,16 +12,28 @@ namespace CopilotTest
             // Try to parse the textbox input as an integer
             if (int.TryParse(textBoxMessage.Text, out int statusCode))
             {
-                 string message = statusCode switch
-                 {
-                     200 => "OK",
-                     400 => "Bad Request",
-                     401 => "Unauthorized",
-                     404 => "Not Found",
-                     500 => "Internal Server Error",
-                     _ => "Unknown Status"
-                 };
-                 textBoxMessage.Text = message;                           
+                string message;
+                if (statusCode == 200)
+                {
+                    message = "OK";
+                }
+                else if (statusCode == 400)
+                {
+                    message = "Bad Request";
+                }
+                else if (statusCode == 404)
+                {
+                    message = "Not Found";
+                }
+                else if (statusCode == 500)
+                {
+                    message = "Internal Server Error";
+                }
+                else
+                {
+                    message = "Unknown Status";
+                }
+                textBoxMessage.Text = message;
             }
             else
             {
