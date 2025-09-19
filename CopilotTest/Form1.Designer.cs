2de5eaa3ddf0de0d1ca9516ae3987a98ae624652
Bernard Kovač <bernard.kovac@aircash.eu>
2025-09-19 21:56:07 +0200

    Add project files.

---
+++ CopilotTest/Form1.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace CopilotTest
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            textBoxMessage = new TextBox();
+            buttonShowMessage = new Button();
+            SuspendLayout();
+            // 
+            // textBoxMessage
+            // 
+            textBoxMessage.Location = new Point(16, 43);
+            textBoxMessage.Multiline = true;
+            textBoxMessage.Name = "textBoxMessage";
+            textBoxMessage.Size = new Size(755, 59);
+            textBoxMessage.TabIndex = 0;
+            // 
+            // buttonShowMessage
+            // 
+            buttonShowMessage.BackColor = Color.FromArgb(128, 255, 128);
+            buttonShowMessage.Location = new Point(16, 124);
+            buttonShowMessage.Name = "buttonShowMessage";
+            buttonShowMessage.Size = new Size(755, 279);
+            buttonShowMessage.TabIndex = 1;
+            buttonShowMessage.Text = "RESOLVE";
+            buttonShowMessage.UseVisualStyleBackColor = false;
+            buttonShowMessage.Click += buttonShowMessage_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(buttonShowMessage);
+            Controls.Add(textBoxMessage);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private TextBox textBoxMessage;
+        private Button buttonShowMessage;
+    }
+}
---
+++ CopilotTest/Form1.cs
@@ -0,0 +1,31 @@
+namespace CopilotTest
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonShowMessage_Click(object sender, EventArgs e)
+        {
+            // Try to parse the textbox input as an integer
+            if (int.TryParse(textBoxMessage.Text, out int statusCode))
+            {
+                string message = statusCode switch
+                {
+                    200 => "OK",
+                    400 => "Bad Request",
+                    404 => "Not Found",
+                    500 => "Internal Server Error",
+                    _ => "Unknown Status"
+                };
+                textBoxMessage.Text = message;
+            }
+            else
+            {
+                textBoxMessage.Text = "Please enter a valid status code (e.g., 200, 400, 404, 500).";
+            }
+        }
+    }
+}
