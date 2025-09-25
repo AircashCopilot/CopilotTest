4bf52f9dc7b84d8e7e7f7f20cc155f4b4febb046
Bernard Kovač <bernard.kovac@aircash.eu>
2025-09-25 10:59:57 +0200

    sdfsfsdf

--- CopilotTest/Form1.cs
+++ CopilotTest/Form1.cs
@@ -13,7 +13,7 @@ namespace CopilotTest
             if (int.TryParse(textBoxMessage.Text, out int statusCode))
             {
                 //this is a string variable
-                 string responseMessage = statusCode switch
+                 string statusMessageText = statusCode switch
                  {
                      200 => "OK",
                      400 => "Bad Request",
@@ -22,7 +22,7 @@ namespace CopilotTest
                      500 => "Internal Server Error",
                      _ => "Unknown Status"
                  };
-                 textBoxMessage.Text = responseMessage;                           
+                 textBoxMessage.Text = statusMessageText;                           
             }
             else
             {
